4a3cb332f31acc23fb27277185a24643731127ca
Ana Julia M Martins <ana_juh17@hotmail.com>
2023-04-15 16:00:00 -0300

    stack window

---
+++ Assets/JS/main.js
@@ -0,0 +1,74 @@
+class Stack{
+    constructor(){
+        this.enterNode= null;
+    }
+    push(data){
+        //Exibir no html
+        let stackDisplay = document.getElementById("stackDisplay");
+        stackDisplay.style.backgroundColor = "red";
+        
+        let exampleP = document.createElement("p");
+
+        //Adiciona novo nó
+        let newNode = {data: data, next: null};
+
+        let auxNode = {data: data, next: null};
+        auxNode = this.enterNode;
+
+        this.enterNode = newNode;
+        this.enterNode.next = auxNode;
+
+        exampleP.innerText(this.enterNode);
+        stackDisplay.appendChild(exampleP);
+
+        return this.enterNode;
+    }
+    pop(){
+        
+        //Retira o no de cima
+        if(!this.isEmpty()){
+            let nodePopped = this.enterNode;
+            this.enterNode = this.enterNode.next;
+           
+            return nodePopped;
+        }else{
+            return null
+        }
+    }
+    top(){
+        //Mostra o no do topo
+        return this.enterNode;
+    }
+    isEmpty(){
+        //Verifica se a pilha está vazia
+        return this.enterNode==null?true:false;
+    }
+    displayAll(){
+        //Exibe a pilha
+        let returnString = "Stack:";
+        let auxNode = this.enterNode;
+
+        if(!this.isEmpty()){
+            while(auxNode!=null){
+                returnString += `value= ${auxNode.data}, `;
+                auxNode = auxNode.next;
+            }
+        }else{
+            returnString = "null";
+        }
+        return returnString;
+    }
+}
+
+
+
+//Instancia objetos
+let myStack = new Stack();
+
+let btnDisplayAllStack = document.getElementById("push");
+btnDisplayAllStack.addEventListener("click", myStack.push);
+
+
+
+
+
